cd0b583dc3b4950d52afb8183e0addaaf9a669fa
张珈豪 <2021300002028@whu.edu.cn>
2024-05-29 09:34:37 +0800

    5/29

--- os/src/config.rs
+++ os/src/config.rs
@@ -7,12 +7,12 @@ pub const PAGE_SIZE: usize = 0x1000;
 pub const PAGE_SIZE_BITS: usize = 0xc;
 
 pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
-pub const HEAP_BUFFER: usize = TRAMPOLINE - PAGE_SIZE;
-pub const TRAP_CONTEXT_BASE: usize = HEAP_BUFFER - PAGE_SIZE;
-
-pub const PROCESS_PRIO_BASE: usize = TRAP_CONTEXT_BASE - PAGE_SIZE*2; // 示例地址，根据需要调整
-pub const THREAD_PRIO_BASE: usize = PROCESS_PRIO_BASE - PAGE_SIZE*2; // 示例地址，根据需要调整
+pub const TRAP_CONTEXT_BASE: usize = TRAMPOLINE - PAGE_SIZE;
 
 pub const BASE_ADDRESS: usize = 0x80200000;
+pub const HEAP_BUFFER: usize = BASE_ADDRESS - PAGE_SIZE;
+
+pub const PROCESS_PRIO_BASE: usize = HEAP_BUFFER - PAGE_SIZE*2; // 示例地址，根据需要调整
+pub const THREAD_PRIO_BASE: usize = PROCESS_PRIO_BASE - PAGE_SIZE*2; // 示例地址，根据需要调整
 
 pub use crate::board::{CLOCK_FREQ, MEMORY_END, MMIO};
